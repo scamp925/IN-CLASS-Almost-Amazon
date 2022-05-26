ae448cb63a3dfab356bac3cc6ed7a658726846cc
Sariah Campopiano <scampopiano925@gmail.com>
2022-05-25 23:10:23 -0500

    Cleaned up unnecessary console.warns from code

--- src/api/mergedData.js
+++ src/api/mergedData.js
@@ -4,10 +4,8 @@ import { getSingleBook } from './bookData';
 const viewBookDetails = (bookFirebaseKey) => new Promise((resolve, reject) => {
   getSingleBook(bookFirebaseKey)
     .then((bookObject) => {
-      console.warn(bookObject);
       getSingleAuthor(bookObject.author_id)
         .then((authorObject) => {
-          console.warn(authorObject);
           resolve({ authorObject, ...bookObject });
         });
     }).catch((error) => reject(error));
